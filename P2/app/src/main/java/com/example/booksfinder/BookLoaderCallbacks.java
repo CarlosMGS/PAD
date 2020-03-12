b585da12cb549a5d5111b5f763bc9e5e8323215f
aruPascual <alvpas01@ucm.es>
2020-03-12 20:43:29 +0100

    P2 0.3

--- P2/app/src/main/java/com/example/booksfinder/BookLoaderCallbacks.java
+++ P2/app/src/main/java/com/example/booksfinder/BookLoaderCallbacks.java
@@ -10,6 +10,9 @@ import androidx.loader.content.Loader;
 public class BookLoaderCallbacks implements  LoaderManager.LoaderCallbacks<String>{
 
 
+    public static final String EXTRA_QUERY = null;
+    public static final String EXTRA_PRINT_TYPE = null;
+
     @NonNull
     @Override
     public Loader<String> onCreateLoader(int id, @Nullable Bundle args) {
--- P2/app/src/main/java/com/example/booksfinder/MainActivity.java
+++ P2/app/src/main/java/com/example/booksfinder/MainActivity.java
@@ -1,18 +1,54 @@
 package com.example.booksfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText editText;
+    EditText editText2;
+    RadioGroup radioGroup;
+    private static final int BOOK_LOADER_ID = -1;
     private BookLoaderCallbacks bookLoaderCallbacks = new BookLoaderCallbacks();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*editText initialization*/
+        editText = (EditText) findViewById(R.id.editText);
+        editText2 = (EditText) findViewById(R.id.editText2);
+
+        /* Loader initialization*/
+        LoaderManager loaderManager = LoaderManager.getInstance(this);
+        if (loaderManager.getLoader(BOOK_LOADER_ID) != null){
+            loaderManager.initLoader(BOOK_LOADER_ID, null, bookLoaderCallbacks);
+        }
     }
 
+    public void searchBooks(View view){
+        /* concat authors names and title */
+        String queryString = editText.getText() + " " + editText2.getText();
+        String printType = new String();
+        int radioButtonId = radioGroup.getCheckedRadioButtonId();
+        RadioButton radioButton;
+        if (radioButtonId != -1){
+            radioButton = (RadioButton) radioGroup.findViewById(radioButtonId);
+            if (radioButton != null){
+                printType = (String) radioButton.getText().toString();
+            }
+        }
 
+        Bundle queryBundle = new Bundle();
+        queryBundle.putString(BookLoaderCallbacks.EXTRA_QUERY, queryString);
+        queryBundle.putString(BookLoaderCallbacks.EXTRA_PRINT_TYPE, printType);
+        LoaderManager.getInstance(this).restartLoader(BOOK_LOADER_ID, queryBundle, bookLoaderCallbacks);
+    }
 }
